6978b7460f7f8425c805025ad1c2aafe1d2370e8
Mikhail Vasilenko <mvasilenko@gmail.com>
2017-09-28 11:25:42 +0300

    change awk to grep in ami id lookup

--- packer-run.sh
+++ packer-run.sh
@@ -1,2 +1,5 @@
 packer build $1 2>&1 | tee output.txt
-tail -2 output.txt | head -2 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' > ami.txt
+# look for AMI id
+#tail -2 output.txt | head -2 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' > ami.txt
+# or
+egrep -m1 -oe 'ami-.{8}' output.txt > ami.txt
